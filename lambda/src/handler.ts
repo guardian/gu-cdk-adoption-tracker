578664e074cb356ab176f622de0209ff887daaac
Jacob Winch <jacob.winch@guardian.co.uk>
2021-04-01 09:45:00 +0100

    Add axios dependency and fetch data from Prism

--- lambda/src/handler.ts
+++ lambda/src/handler.ts
@@ -1,6 +1,7 @@
 import { Octokit } from "@octokit/rest";
 import { RestEndpointMethodTypes } from "@octokit/plugin-rest-endpoint-methods/dist-types/generated/parameters-and-response-types";
 import CloudWatch from "aws-sdk/clients/cloudwatch";
+import axios from "axios";
 
 if (!process.env["API_TOKEN"]) {
   throw new Error("No API_TOKEN envar for GitHub");
@@ -38,23 +39,50 @@ export async function handler() {
     value: reposUsingGuCdk.data.total_count,
   });
 
-  const usingAwsCdk = await octokit.search.code({
+  const reposUsingAwsCdk = await octokit.search.code({
     q: `user:guardian filename:package.json "@aws-cdk"`,
   });
-  console.log(`${usingAwsCdk.data.total_count} using @aws-cdk/core`);
+  console.log(`${reposUsingAwsCdk.data.total_count} using @aws-cdk/core`);
   await putMetric({
     name: "Repositories using @aws-cdk/core",
-    value: usingAwsCdk.data.total_count,
+    value: reposUsingAwsCdk.data.total_count,
   });
 
-  const usingCfn = await octokit.search.code({
+  const reposUsingCloudFormation = await octokit.search.code({
     q: `user:guardian filename:"cloudformation.yaml" filename:"cloudformation.yml" filename:"cfn.yaml" filename:"cfn.yml"`,
   });
-  console.log(`${usingCfn.data.total_count} using cloudformation`);
+  console.log(`${reposUsingCloudFormation.data.total_count} using cloudformation`);
   await putMetric({
     name: "Repositories using Cloudformation",
-    value: usingCfn.data.total_count,
+    value: reposUsingCloudFormation.data.total_count,
   });
+
+  interface AppWithCoreTags {
+    app: string;
+    stack: string;
+    stage: string;
+    guCdkVersion: string;
+  }
+  
+  const prismResponse = await axios.get(`${process.env.PRISM_URL}/apps-with-cdk-version`);
+  const allApps: AppWithCoreTags[] = prismResponse.data.data['apps-with-cdk-version']
+  const appsUsingGuCdk = allApps.filter(app => app.guCdkVersion != "n/a")
+
+  const numberOfAppsAlreadyUsingGuCdk = appsUsingGuCdk.length
+  const numberOfAppsLeftToMigrate = allApps.length - appsUsingGuCdk.length
+
+  console.log(`Prism data shows that ${numberOfAppsAlreadyUsingGuCdk} apps are using @guardian/cdk.`)
+  await putMetric({
+    name: "Apps using @guardian/cdk",
+    value: numberOfAppsAlreadyUsingGuCdk,
+  });
+
+  console.log(`Prism data shows that there are ${numberOfAppsLeftToMigrate} apps left to migrate.`)
+  await putMetric({
+    name: "Apps which need to migrate to @guardian/cdk",
+    value: numberOfAppsLeftToMigrate,
+  });
+
 }
 
 if (require.main === module) {
